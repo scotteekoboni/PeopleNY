c59eb7ed53116da579213dc99132f454d7d2660f
Scott Guidoboni <scottguido55@gmail.com>
2018-01-23 09:04:50 -0500

    final submission ass 1

--- PeopleofNY/PeopleofNY/ViewControllers/PhotosViewController.swift
+++ PeopleofNY/PeopleofNY/ViewControllers/PhotosViewController.swift
@@ -9,17 +9,42 @@ import UIKit
 
 import AlamofireImage
 
-class PhotosViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+class PhotosViewController: UIViewController, UITableViewDataSource, UITableViewDelegate{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        print("Number of posts: \(posts.count)")
+        return posts.count
+    }
     
-    
-  
-    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let post = posts[indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PhotoCell", for: indexPath) as! PhotoCell
+        if let photos = post["photos"] as? [[String: Any]] {
+            // photos is NOT nil, we can use it!
+            // TODO: Get the photo url
+            
+            // 1.
+            let photo = photos[0]
+            // 2.
+            let originalSize = photo["original_size"] as! [String: Any]
+            // 3.
+            let urlString = originalSize["url"] as! String
+            // 4.
+            let url = URL(string: urlString)!
+            
+            
+            cell.picView.af_setImage(withURL: url)
+            
+            
+            // let cell = tableView.dequeueReusableCell(withIdentifier: "PhotoCell", for: indexPath) as! PhotoCell
+            //cell.textLabel?.text = "This is row \(indexPath.row)"
+            //self.tableView.reloadData()
+            
+        }
+        return cell
+    }
     
 
-   
-    
-    @IBOutlet weak var photoViewController: UITableView!
-    
+    @IBOutlet weak var tableView: UITableView! //no outlet
 
     var posts: [[String: Any]] = []
     
@@ -29,8 +54,10 @@ class PhotosViewController: UIViewController, UITableViewDataSource, UITableView
         tableView.delegate = self
         tableView.dataSource = self
         
+        fetchPicture()
+    }
         
-        
+    func fetchPicture() {
         // Network request snippet
         let url = URL(string: "https://api.tumblr.com/v2/blog/humansofnewyork.tumblr.com/posts/photo?api_key=Q6vHoaVm5L1u2ZAW1fqv3Jw48gFzYVg9P0vH0VHl3GVy6quoGV")!
         let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
@@ -38,8 +65,8 @@ class PhotosViewController: UIViewController, UITableViewDataSource, UITableView
         let task = session.dataTask(with: url) { (data, response, error) in
             if let error = error {
                 print(error.localizedDescription)
-            } else if let data = data,
-                let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as? [String: Any] {
+            } else if let data = data {
+                let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
                 print(dataDictionary)
                 
                 // TODO: Get the posts and store in posts property
@@ -49,57 +76,22 @@ class PhotosViewController: UIViewController, UITableViewDataSource, UITableView
                 self.posts = responseDictionary["posts"] as! [[String: Any]]
                 
                 // TODO: Reload the table view
-                
+                self.tableView.reloadData()
                 
             }
         }
         task.resume()
-        
-        
-        func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-            return 5
-        }
-        
-        func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-            
-            let post = posts[indexPath.row]
-            
-            if let photos = post["photos"] as? [[String: Any]] {
-                // photos is NOT nil, we can use it!
-                // TODO: Get the photo url
-            }
-
-            
-            let cell = tableView.dequeueReusableCell(withIdentifier: "PhotoCell") as! PhotoCell
-
-            // 1.
-            let photo = photos[0]
-            // 2.
-            let originalSize = photo["original_size"] as! [String: Any]
-            // 3.
-            let urlString = originalSize["url"] as! String
-            // 4.
-            let url = URL(string: urlString)
-            
-            cell.photoImageView.af_setImage(withURL: url!)
-            
-           // let cell = tableView.dequeueReusableCell(withIdentifier: "PhotoCell", for: indexPath) as! PhotoCell
-            cell.textLabel?.text = "This is row \(indexPath.row)"
-            self.tableView.reloadData()
-            return cell
-        }
-        
-        
-        
-
-        // Do any additional setup after loading the view.
     }
+        
+
+
+        
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
 
     /*
     // MARK: - Navigation
